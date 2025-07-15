6bf76d9500adee4bb2d75bf3d418cd6f0e3863cd
afreakk <laderud@hotmail.com>
2025-07-15 14:37:17 +0200

    update 1pw and nix installer

--- packer/linux/scripts/install-1password.sh
+++ packer/linux/scripts/install-1password.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 echo "Installing 1password-cli"
-wget https://cache.agilebits.com/dist/1P/op2/pkg/v2.18.0-beta.01/op_linux_amd64_v2.18.0-beta.01.zip
-unzip op_linux_amd64_v2.18.0-beta.01.zip
-rm op.sig
-rm op_linux_amd64_v2.18.0-beta.01.zip
-chmod +x op
-sudo mv op /usr/local/bin/op
-op -v
+wget "https://cache.agilebits.com/dist/1P/op2/pkg/v2.31.1/op_linux_amd64_v2.31.1.zip" -O op.zip && \
+unzip -d op op.zip && \
+sudo mv op/op /usr/local/bin/ && \
+rm -r op.zip op && \
+sudo groupadd -f onepassword-cli && \
+sudo chgrp onepassword-cli /usr/local/bin/op && \
+sudo chmod g+s /usr/local/bin/op
--- packer/linux/scripts/install-nix.sh
+++ packer/linux/scripts/install-nix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 echo "Installing nix"
-sh <(curl -L https://nixos.org/nix/install) --daemon
+curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm
 echo ". /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh" | sudo tee -a /etc/buildkite-agent/hooks/pre-command
